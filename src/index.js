8a47823e9a01319198aee9002a60e34ed9f010a1
vtgal <vtskyle101@gmail.com>
2024-09-05 12:32:02 -0500

    Inject the forecast HTML from javascript.

--- src/index.js
+++ src/index.js
@@ -51,7 +51,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+    
+    days.forEach(function (day) {
+        forecastHtml =
+        forecastHtml +
+         `
+        <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-temperatures"> 
+                <div class="weather-forecast-temperature">
+                    <strong>15°</strong>
+                </div>
+                 <div class="weather-forecast-temperature">9°</div>
+             </div>
+            </div>
+        `;
+    });
+    let forecastElement = document.querySelector("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+    }
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
