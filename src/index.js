fcb874a61fc35883864471bc11227f0bd11a5d25
vtgal <vtskyle101@gmail.com>
2024-09-05 13:37:33 -0500

    Updated my css, and injected more function

--- src/index.js
+++ src/index.js
@@ -52,6 +52,12 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+}
 
 function getForecast(city) {
     let apiKey = "6e4744f50a4o233acde7cb8355a181t3";
@@ -60,25 +66,28 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-
-    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
     let forecastHtml = "";
     
-    days.forEach(function (day) {
+    response.data.daily.forEach(function (day, index) {
+        if (index < 5) {
         forecastHtml =
-        forecastHtml +
-         `
+          forecastHtml +
+          `
         <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">⛅</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+            <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
             <div class="weather-forecast-temperatures"> 
                 <div class="weather-forecast-temperature">
-                    <strong>15°</strong>
+                    <strong>${Math.round(day.temperature.maximum)}°</strong>
                 </div>
-                 <div class="weather-forecast-temperature">9°</div>
+                 <div class="weather-forecast-temperature">${Math.round(
+                   day.temperature.minimum
+                 )}°</div>
              </div>
             </div>
         `;
+        }
     });
     let forecastElement = document.querySelector("#forecast");
     forecastElement.innerHTML = forecastHtml;
